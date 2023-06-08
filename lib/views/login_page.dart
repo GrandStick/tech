71f5864bff44c6728c04ae6afbfc107bc5717ed9
GrandStick <moreaux.sebastien@gmail.com>
2023-06-08 17:25:51 +0200

    retirer moke

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -29,7 +29,7 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void checkLoginStatus() async {
-    SharedPreferences.setMockInitialValues({});
+    //SharedPreferences.setMockInitialValues({});
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
 
